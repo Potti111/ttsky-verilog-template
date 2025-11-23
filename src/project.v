0fb80efc51c3fc7c911090c6be9badbb4f886481
Potti111 <pototzki.mirko@rbz-steinburg.de>
2025-11-23 23:10:36 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,38 +1,22 @@
 `default_nettype none
 
-module tt_um_vga_example(
-   input clk,
-   input ena,
-   input rst_n,
-  input reset,
-  output reg clk_div2,
-  output reg clk_div4,
-  output reg clk_div8,
-  output reg clk_div16,
-  output reg [3:0] cnt,
-  output reg clk_invert
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
-  assign clk_invert = ~clk;
-  // simple ripple clock divider
-  
-  always @(posedge clk)
-    clk_div2 <= reset ? 0 : ~clk_div2;
-
-  always @(posedge clk_div2)
-    clk_div4 <= ~clk_div4;
-
-  always @(posedge clk_div4)
-    clk_div8 <= ~clk_div8;
-
-  always @(posedge clk_div8)
-    clk_div16 <= ~clk_div16;
-  
-  always @(posedge clk_div2)
-    if (reset)
-      cnt <= 0;
-  else
-    cnt <= cnt +1;
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule

a5b54c9f1410e23c20ea4c0ff11697f1de11232a
Potti111 <pototzki.mirko@rbz-steinburg.de>
2025-11-23 22:59:02 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,53 +1,36 @@
 `default_nettype none
 
 module tt_um_vga_example(
-  input wire [7:0] ui_in,    // Dedizierte Eingänge
-  output wire [7:0] uo_out,  // Dedizierte Ausgänge
-  input wire [7:0] uio_in,   // IOs: Eingangs-Pfad
-  output wire [7:0] uio_out, // IOs: Ausgangs-Pfad
-  output wire [7:0] uio_oe,  // IOs: Enable-Pfad (aktiv High: 0=Eingang, 1=Ausgang)
-  input wire ena,            // immer 1, solange das Design mit Strom versorgt ist - kann ignoriert werden
-  input wire clk,            // Takt
-  input wire rst_n           // reset_n - Low = Reset
+   input clk,
+  input reset,
+  output reg clk_div2,
+  output reg clk_div4,
+  output reg clk_div8,
+  output reg clk_div16,
+  output reg [3:0] cnt,
+  output reg clk_invert
 );
 
-  // VGA-Signale deklarieren
-  wire hsync;
-  wire vsync;
-  wire [1:0] R;
-  wire [1:0] G;
-  wire [1:0] B;
-  wire video_active;
-  wire [9:0] pix_x;
-  wire [9:0] pix_y;
 
-  // TinyVGA PMOD: Die VGA-Ausgänge zuweisen
-  assign uo_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+  assign clk_invert = ~clk;
+  // simple ripple clock divider
+  
+  always @(posedge clk)
+    clk_div2 <= reset ? 0 : ~clk_div2;
 
-  // Ungenutzte Ausgänge auf 0 setzen
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  always @(posedge clk_div2)
+    clk_div4 <= ~clk_div4;
 
-  // Unterdrücke Warnungen für ungenutzte Signale
-  wire _unused_ok = &{ena, ui_in, uio_in};
+  always @(posedge clk_div4)
+    clk_div8 <= ~clk_div8;
 
-  // Ein instanzierte hvsync_generator-Modul, das die VGA-Signale erzeugt
-  hvsync_generator hvsync_gen(
-    .clk(clk),
-    .reset(~rst_n),
-    .hsync(hsync),
-    .vsync(vsync),
-    .display_on(video_active),
-    .hpos(pix_x),
-    .vpos(pix_y)
-  );
-
-  // Flächenkoordinaten für das rote Quadrat definieren
-  wire square_on = (pix_x >= 200 && pix_x < 440) && (pix_y >= 160 && pix_y < 320);
-
-  // Die Farben zuweisen; Rot ist aktiviert wenn das Quadrat sichtbar ist
-  assign R = video_active && square_on ? 2'b11 : 2'b00; // Volles Rot
-  assign G = 2'b00; // Kein Grün
-  assign B = 2'b00; // Kein Blau
+  always @(posedge clk_div8)
+    clk_div16 <= ~clk_div16;
+  
+  always @(posedge clk_div2)
+    if (reset)
+      cnt <= 0;
+  else
+    cnt <= cnt +1;
 
 endmodule
